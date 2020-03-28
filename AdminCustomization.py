efc54069d3aae0cd7fcb69960aecc72818d01c25
Arun <arunhari3210@gmail.com>
2020-03-28 19:58:27 +0530

    Added Admin Customization example

---
+++ AdminCustomization.py
@@ -0,0 +1,21 @@
+class UserAdmin(BaseUserAdmin):
+    form = UserAdminChangeForm
+    add_form = UserAdminCreationForm
+    list_display = ('email', 'admin')
+    list_filter = ('admin',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': (extra_field,)}),
+        ('Permissions', {'fields': ('admin',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')}
+         ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+admin.site.register(User, UserAdmin)
